64f6823a93c2121ff02a4a0880fb64645026087e
yasiru <yasirunet@gmail.com>
2020-12-17 23:58:21 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
